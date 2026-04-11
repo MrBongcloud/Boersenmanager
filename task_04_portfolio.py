79d097ebb83b4302095d104a47b16330717ad01c
RaGa <gangl.raphael@tha.de>
2026-04-11 17:17:23 +0200

    add ing class share

---
+++ task_04_portfolio.py
@@ -0,0 +1,21 @@
+#Imports
+
+#Functions and Modules
+class Portfolio:    #ceation of class
+
+    def __init__(self, name, basepath): #set name, basepath, capital 
+        self.name = name
+        self.base_path = basepath
+        self.capital = 0.0
+
+    def change_available_capital(self): #addor remove capital 
+        pass
+
+    def load_all_shares(self):
+        pass
+
+# testing
+
+if __name__ == '__main__':
+    print('Hello')
+
